d2f4d04c021c709de934b155ffb4151e263fb7de
Alfredo Barillas <alfredoeb96@gmail.com>
2026-09-22 15:25:37 -0500

    [activate newton] created function to force newton app to be in focus

--- helper_func.py
+++ helper_func.py
@@ -1,10 +1,41 @@
+import psutil
+
 from fastapi import HTTPException
+from pywinauto import Desktop
 
 import newton_gui
 
+# Check if newton is running
 def require_newton_running():
     if not newton_gui.is_newton_running():
         raise HTTPException(
             status_code=503,
             detail="Newton.exe is not running"
         )
+        
+# Grab the PID of newton app
+def get_newton_pid() -> int:
+    for process in psutil.process_iter(["pid", "name"]):
+        try:
+            if process.info["name"] == "Newton.exe":
+                return process.info["pid"]
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            pass
+
+    raise RuntimeError("Newton.exe is not running")
+
+# Make the newton app top level (must be running app or terminal as admin)
+def activate_newton() -> None:
+    pid = get_newton_pid()
+
+    windows = Desktop(backend="win32").windows()
+
+    for window in windows:
+        try:
+            if window.process_id() == pid and window.window_text() == "Newton":
+                window.set_focus()
+                return
+        except Exception:
+            pass
+
+    raise RuntimeError("Newton window not found")
